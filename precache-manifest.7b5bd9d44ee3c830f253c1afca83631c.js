70d5aa29bf1fb046783e764b0028d37e546af918
amir <amir2541@gmail.com>
2020-03-13 23:14:20 +0600

    Updates

---
+++ precache-manifest.7b5bd9d44ee3c830f253c1afca83631c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9d6d7035666cb09be25b3110c98459",
+    "url": "/mancity/index.html"
+  },
+  {
+    "revision": "a8ba147b19566f704b50",
+    "url": "/mancity/static/css/main.23c53f1a.chunk.css"
+  },
+  {
+    "revision": "737789c8a9bb1647c769",
+    "url": "/mancity/static/js/2.b4c7a87c.chunk.js"
+  },
+  {
+    "revision": "fff893798278dc57ce07f24dd8c43a2b",
+    "url": "/mancity/static/js/2.b4c7a87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ba147b19566f704b50",
+    "url": "/mancity/static/js/main.017a3e47.chunk.js"
+  },
+  {
+    "revision": "51bd94929c1ae226358f",
+    "url": "/mancity/static/js/runtime-main.0580de4b.js"
+  },
+  {
+    "revision": "5432bcd5600bfba5ec63967d7efb05b9",
+    "url": "/mancity/static/media/Otamendi.5432bcd5.png"
+  },
+  {
+    "revision": "34e300e4fce09e33d958a059176f2b98",
+    "url": "/mancity/static/media/Raheem.34e300e4.png"
+  },
+  {
+    "revision": "0896fc7df45471573674f953a3c736ff",
+    "url": "/mancity/static/media/Vincent.0896fc7d.png"
+  },
+  {
+    "revision": "6de3706ade22bf1989b3853dda736986",
+    "url": "/mancity/static/media/featured_player.6de3706a.png"
+  },
+  {
+    "revision": "dd26db2771a362bbd922fa4686b5218a",
+    "url": "/mancity/static/media/jersey.dd26db27.jpg"
+  },
+  {
+    "revision": "1d5b64ec5f8fe8089fd9370edd244ac4",
+    "url": "/mancity/static/media/manchester_city_logo.1d5b64ec.png"
+  }
+]);
